c7e73b8b188afcab85448b52a56ac6250790e584
Jason Thompson <jason@jthompson.ca>
2014-05-09 10:35:22 -0400

    Refactor for readability
    - Divide conversions into multiple tasks
    - Each converter class has a #convert method

--- binary.rb
+++ binary.rb
@@ -1,5 +1,5 @@
 module BinaryConversion
-  DIGIT_VALUES ||= [128, 64, 32, 16, 8, 4, 2, 1]
+  PLACE_VALUES ||= [128, 64, 32, 16, 8, 4, 2, 1]
 
   class BinaryToDecimalConverter
     attr_reader :value
@@ -9,14 +9,14 @@ module BinaryConversion
     end
 
     def convert
-      map_digits_to_values.reduce(:+)
+      mult_digits_by_place_values.reduce(:+)
     end
 
     private
 
-    def map_digits_to_values
+    def mult_digits_by_place_values
       binary_digits.map.with_index do |digit, v|
-        digit.to_i * DIGIT_VALUES[v]
+        digit.to_i * PLACE_VALUES[v]
       end
     end
 
@@ -25,32 +25,32 @@ module BinaryConversion
     end
   end
 
-  class DecimalToBinaryConverter 
+  class DecimalToBinaryConverter
     attr_reader :value
 
     def initialize(decimal_num)
       @value = decimal_num.to_i
+      @current_value = @value
     end
 
-    def convert 
-      map_values_to_digits.join
+    def convert
+      map_place_values_to_digits.join
     end
 
     private
 
-    def zero_or_one(binary_value)
-      current_value ||= value
-      if current_value >= binary_value
-        current_value = current_value - binary_value
-        return "1"
-      else
-        return "0"
+    def calculate_digit_value(decimal_value, place_value)
+      result = "0"
+      if(decimal_value >= place_value)
+        result = "1"
+        @current_value = decimal_value - place_value
       end
+      result
     end
 
-    def map_values_to_digits
-      DIGIT_VALUES.map do |v|
-        zero_or_one(v)
+    def map_place_values_to_digits
+      PLACE_VALUES.map do |place_value|
+        calculate_digit_value(@current_value, place_value)
       end
     end
   end
